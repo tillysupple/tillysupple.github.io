0c73af35b748743a3d084b52f2613efd84055a04
Tilly <mbs115@ic.ac.uk>
2019-01-08 20:50:49 +0000

    changes to format of page

--- working.js
+++ working.js
@@ -2,6 +2,26 @@
 var ctx = document.getElementById("myChart");
 
 
+function updateConfigAsNewObject(chart) {
+    chart.options = {
+        responsive: true,
+        title:{
+            display:true,
+            text: 'Chart.js'
+        },
+        scales: {
+            xAxes: [{
+                display: true
+            }],
+            yAxes: [{
+                display: true
+            }]
+        }
+    }
+    chart.update();
+}
+var white = 'rgb(255,255,255)';
+
 var myLineChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -9,22 +29,8 @@ var myLineChart = new Chart(ctx, {
         datasets: [{
             label: '# of Votes',
             data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
+            backgroundColor: white,
+            borderColor: white,
             borderWidth: 1
         }]
     },
